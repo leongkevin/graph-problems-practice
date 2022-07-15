4aef75564eb95b4e7ef21ea5415d6372164811f5
jack <jzhang319@gmail.com>
2022-07-14 20:36:22 -0400

    updated

--- island.js
+++ island.js
@@ -14,16 +14,16 @@ function getNeighbors(row, col, graph) {
   if (row > 0 && graph[row - 1][col] === 1) {
     neighborArr.push([row - 1, col]);
   }
-  if (row < graph.length - 1 && graph[row + 1][col] === 1) {
+  if (row <= graph.length - 1 && graph[row + 1][col] === 1) {
     neighborArr.push([row + 1, col]);
   }
   if (col > 0 && graph[row][col - 1] === 1) {
     neighborArr.push([row, col - 1]);
   }
-  if (col < graph[col].length - 1 && graph[row][col + 1] === 1) {
+  if (col <= graph[col].length - 1 && graph[row][col + 1] === 1) {
     neighborArr.push([row, col + 1]);
   }
-  console.log(neighborArr + ` <---arr`);
+  console.log(neighborArr + ` <--- console.log`);
   return neighborArr;
 }
 
