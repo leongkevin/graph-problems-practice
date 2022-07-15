ce601849a661a3fc3cf2c7c2134557edcbf47f8f
jack <jzhang319@gmail.com>
2022-07-14 20:24:37 -0400

    finish 1st problem

--- island.js
+++ island.js
@@ -1,30 +1,30 @@
 function getNeighbors(row, col, graph) {
-	// Check top
+  // Check top
 
-	// Check bottom
+  // Check bottom
 
-	// Check left
+  // Check left
 
-	// Check right
+  // Check right
 
-	// Return neighbors
+  // Return neighbors
 
-	// Your code here
-	const neighborArr = [];
-	if (row > 0 && graph[row - 1][col] === 1) {
-		neighborArr.push([row - 1, col]);
-	}
-	if (row < row.length && graph[row + 1][col] === 1) {
-		neighborArr.push([row + 1, col]);
-	}
-	if (col > 0 && graph[row][col - 1] === 1) {
-		neighborArr.push([row, col - 1]);
-	}
-	if (col < col.length && graph[row][col + 1] === 1) {
-		neighborArr.push([row, col + 1]);
-	}
-  console.log(neighborArr + ` <---` )
-	return neighborArr;
+  // Your code here
+  const neighborArr = [];
+  if (row > 0 && graph[row - 1][col] === 1) {
+    neighborArr.push([row - 1, col]);
+  }
+  if (row < graph.length - 1 && graph[row + 1][col] === 1) {
+    neighborArr.push([row + 1, col]);
+  }
+  if (col > 0 && graph[row][col - 1] === 1) {
+    neighborArr.push([row, col - 1]);
+  }
+  if (col < graph[col].length - 1 && graph[row][col + 1] === 1) {
+    neighborArr.push([row, col + 1]);
+  }
+  console.log(neighborArr + ` <---arr`);
+  return neighborArr;
 }
 
 // matrix = [
@@ -35,19 +35,19 @@ function getNeighbors(row, col, graph) {
 // ]
 
 function islandSize(row, col, graph) {
-	// Create a visited set to store visited nodes
-	// Create a stack, put the starting node in the stack
-	// Put the stringified starting node in visited
-	// Initialize size to 0
-	// While the stack is not empty,
-	// Pop the first node
-	// DO THE THING (increment size by 1)
-	// Then push all the UNVISITED neighbors on top of the stack
-	// and mark them as visited
-	// HINT: This is what your helper function `getNeighbors` is for
-	// HINT: Remember, you're storing your visited nodes as strings!
-	// return size
-	// Your code here
+  // Create a visited set to store visited nodes
+  // Create a stack, put the starting node in the stack
+  // Put the stringified starting node in visited
+  // Initialize size to 0
+  // While the stack is not empty,
+  // Pop the first node
+  // DO THE THING (increment size by 1)
+  // Then push all the UNVISITED neighbors on top of the stack
+  // and mark them as visited
+  // HINT: This is what your helper function `getNeighbors` is for
+  // HINT: Remember, you're storing your visited nodes as strings!
+  // return size
+  // Your code here
 }
 
 module.exports = [getNeighbors, islandSize];
